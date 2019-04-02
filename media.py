511c7025349d4a530a4be6721bddc73395670df2
Shrava28 <49205816+Shrava28@users.noreply.github.com>
2019-04-02 23:37:25 +0530

    Add files via upload

---
+++ media.py
@@ -0,0 +1,31 @@
+import webbrowser
+
+
+class Movie():
+    """
+    This class provides a way to store movie related information
+    """
+    valid_ratings = ["G", "PG", "PG-13", "R"]
+
+    def __init__(
+            self, movie_title, movie_storyline, poster_image, 
+            trailer_youtube):
+        """ This docstring should explain constructor methods input
+            and output
+
+            :param movie_title: string
+            :param movie_storyline: string
+            :param poster_image: string
+            :param trailer_youtube: string
+        """            
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
+        """ This docstring should explain what the show_trailer
+            function does
+            input and output
+        """
+        webbrowser.open(self.trailer_youtube_url)
